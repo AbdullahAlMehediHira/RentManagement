58856753723cd86543897dc6f8980debaf5fedea
AbdullahAlMehediHira <adullah.hira@northsouth.edu>
2019-11-05 03:11:29 +0600

    activity added

--- app/src/main/java/com/example/rentapplication/MainActivity.java
+++ app/src/main/java/com/example/rentapplication/MainActivity.java
@@ -1,9 +1,53 @@
 package com.example.rentapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public Button button;
+    public void land(){
+        button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent x = new Intent(MainActivity.this,LandOwnerActv.class);
+
+                startActivity(x);
+
+            }
+        });
+
+    }
+
+    public Button button2;
+    public void tent(){
+        button2=(Button)findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent y = new Intent(MainActivity.this,Tenant.class);
+
+                startActivity(y);
+
+            }
+        });
+
+    }
+
+
+
+
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
