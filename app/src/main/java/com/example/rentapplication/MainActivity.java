0ee7b0e3e20c47823481ee657127d1aa29aa0976
AbdullahAlMehediHira <adullah.hira@northsouth.edu>
2019-11-05 03:14:32 +0600

    few changes

--- app/src/main/java/com/example/rentapplication/MainActivity.java
+++ app/src/main/java/com/example/rentapplication/MainActivity.java
@@ -39,20 +39,12 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
-
-
-
-
-
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tent();
+        land();
 
     }
 }
