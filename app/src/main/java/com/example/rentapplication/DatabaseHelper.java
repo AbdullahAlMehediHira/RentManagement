49e00ea48dcf87ea40accb0e17f641ce9813738d
AbdullahAlMehediHira <adullah.hira@northsouth.edu>
2019-11-12 01:47:21 +0600

    connection setup

---
+++ app/src/main/java/com/example/rentapplication/DatabaseHelper.java
@@ -0,0 +1,66 @@
+package com.example.rentapplication;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+
+
+    public DatabaseHelper(Context context) {
+
+        super(context,"Register.db",null,1);
+
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        // creating tables
+        sqLiteDatabase.execSQL("Create table user(name text primary key, number text)");
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("drop table if exists user");//drops table if exists
+
+        onCreate(sqLiteDatabase);
+    }
+    //inserting in database of owners
+    public Boolean insert( String name, String number ){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name",name);
+        contentValues.put("number",number);
+        long ins = sqLiteDatabase.insert("user",null,contentValues);
+        if(ins==-1) return false;
+        else return true;
+
+
+    }
+
+    // checking if name exists of owners
+    public Boolean checkmate(String name){
+        SQLiteDatabase  sqLiteDatabase = this.getReadableDatabase();
+        Cursor cursor = sqLiteDatabase.rawQuery("Select * from user where name =?",new String[]{name});
+        if(cursor.getCount()>0) return false;
+        else return true;
+
+    }
+
+    //checking name and number
+    public Boolean namenumber(String name , String number){
+        SQLiteDatabase  sqLiteDatabase = this.getReadableDatabase();
+        Cursor cursor = sqLiteDatabase.rawQuery("Select * from user where name =? and number=?",new String[]{name,number});
+        if(cursor.getCount()>0) return true;
+        else return false;
+
+    }
+
+
+}
--- app/src/main/java/com/example/rentapplication/LandOwnerActv.java
+++ app/src/main/java/com/example/rentapplication/LandOwnerActv.java
@@ -1,13 +1,85 @@
 package com.example.rentapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LandOwnerActv extends AppCompatActivity {
+
+
+    EditText e1,e2,e3;
+    DatabaseHelper sqLiteDatabase;
+    Button Button3;
+
+
+    public Button login;
+    public void log(){
+        login=(Button)findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent s = new Intent(LandOwnerActv.this,Owner_log.class);
+
+                startActivity(s);
+
+            }
+        });
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_land_owner_actv);
+
+
+        log();
+
+        sqLiteDatabase = new DatabaseHelper(this);
+        e1=(EditText)findViewById(R.id.name);
+        e2=(EditText)findViewById(R.id.number);
+        e3=(EditText)findViewById(R.id.cnumber);
+        Button3=(Button)findViewById(R.id.register);
+        Button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s1 = e1.getText().toString();
+                String s2 = e2.getText().toString();
+                String s3 = e3.getText().toString();
+                if (s1.equals("") || s2.equals("") || s3.equals("")) {
+                    Toast.makeText(getApplicationContext(), "স্থান পূরণ করুন", Toast.LENGTH_SHORT).show();
+
+                }
+
+                else {
+                    if (s2.equals(s3)) {
+                        Boolean checkmate = sqLiteDatabase.checkmate(s1);
+                        if (checkmate == true) {
+                            Boolean insert = sqLiteDatabase.insert(s1, s2);
+                            if (insert == true) {
+                                Toast.makeText(getApplicationContext(), "সাফল্যের সাথে নিবন্ধিত", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "নাম ইতিমধ্যে বিদ্যমান", Toast.LENGTH_SHORT).show();
+                        }
+
+
+                    }
+                    //Toast.makeText(getApplicationContext(),"সংখ্যা মেলে না",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+
+
+        });
+
+
+
     }
 }
