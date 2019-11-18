d3c0606a4efd054c3e9a07813a1006af001baf52
AbdullahAlMehediHira <adullah.hira@northsouth.edu>
2019-11-18 23:07:24 +0600

    log_home

---
+++ app/src/main/java/com/example/rentapplication/Tenant_home_page.java
@@ -0,0 +1,13 @@
+package com.example.rentapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tenant_home_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tenant_home_page);
+    }
+}
